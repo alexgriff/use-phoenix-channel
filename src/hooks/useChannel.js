b45377d417d6bc78ae55eb0d08a4782807fac654
Alex Griffith <alexalexgriffith@gmail.com>
2018-11-18 22:39:30 -0500

    Leave channel when unmounting

--- src/hooks/useChannel.js
+++ src/hooks/useChannel.js
@@ -6,9 +6,9 @@ const useChannel = (channelTopic, reducer, initialState) => {
   const [state, dispatch] = useReducer(reducer, initialState)
   const [broadcast, setBroadcast] = useState(mustJoinChannelWarning)
 
-  useEffect(() => {
+  useEffect(() => (
     joinChannel(socket, channelTopic, dispatch, setBroadcast)
-  }, [channelTopic])
+  ), [channelTopic])
 
   return [state, broadcast]
 }
@@ -16,20 +16,28 @@ const useChannel = (channelTopic, reducer, initialState) => {
 const joinChannel = (socket, channelTopic, dispatch, setBroadcast) => {
   const channel = socket.channel(channelTopic, {client: 'browser'})
 
-  socket.onMessage(({ event, topic, payload }) => {
+  const handleMessage = ({ event, topic, payload }) => {
     if (topic === channelTopic) {
       dispatch({ event, payload })
     }
-  })
+  }
+
+  socket.onMessage(handleMessage)
 
   channel.join()
     .receive("ok", ({messages}) =>  console.log('successfully joined channel', messages || ''))
     .receive("error", ({reason}) => console.error('failed to join channel', reason))
 
   setBroadcast(() => channel.push.bind(channel))
+
+  return () => {
+    socket.stateChangeCallbacks.message = socket.stateChangeCallbacks.message.filter(cb => cb !== handleMessage)
+    channel.leave()
+  }
 }
 
 const mustJoinChannelWarning = () => (
   () => console.error(`useChannel broadcast function cannot be invoked before the channel has been joined`)
 )
+
 export default useChannel
